ea275e89a2a1feb33f3165f4ef1a986dbdbfb6cf
AntonioDePau <antoine301191@hotmail.fr>
2020-09-19 20:39:32 +0100

    Removed extra space again

--- ConsoleProject/src/services/TextureConverter.cs
+++ ConsoleProject/src/services/TextureConverter.cs
@@ -166,7 +166,7 @@ namespace ConsoleProject.Services {
                 file.AddRange(BitConverter.GetBytes((short)(texture.BitsPerPixel == 8 ? 8447 : 767)).ToList());
                 file.AddRange(BitConverter.GetBytes(texture.PaletteOffset).ToList());
             }
-            if (file.Count % 16 != 0) file.AddRange(Enumerable.Repeat((byte) 0x00, 16 - (file.Count % 16)).ToList());
+            if (file.Count % 16 != 0) file.AddRange(Enumerable.Repeat((byte)0x00, 16 - (file.Count % 16)).ToList());
 
             header.FileNamesOffset = file.Count;
             for (int i = 0; i < images.Count; i++) {
@@ -174,7 +174,7 @@ namespace ConsoleProject.Services {
                 file.AddRange(System.Text.Encoding.UTF8.GetBytes(texture.Name).ToList());
                 if (i < images.Count) file.Add(0);
             }
-            if (file.Count % 16 != 0) file.AddRange(Enumerable.Repeat((byte) 0x00, 16 - (file.Count % 16)).ToList());
+            if (file.Count % 16 != 0) file.AddRange(Enumerable.Repeat((byte)0x00, 16 - (file.Count % 16)).ToList());
 
             file.RemoveRange(0, 4);
             file.InsertRange(0, System.Text.Encoding.UTF8.GetBytes(header.FileExtension).ToList());
